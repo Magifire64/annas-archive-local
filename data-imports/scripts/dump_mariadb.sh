cd649d59e3c1e230b274502f111dc5555d3dd813
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2025-01-18 00:00:00 +0000

    zzz

--- data-imports/scripts/dump_mariadb.sh
+++ data-imports/scripts/dump_mariadb.sh
@@ -12,7 +12,7 @@ cd /temp-dir
 rm -rf /exports/mariadb
 mkdir /exports/mariadb
 cd /exports/mariadb
-mydumper --threads 32 --omit-from-file /app/data-imports/scripts/dump_mariadb_omit_tables.txt --exit-if-broken-table-found --tz-utc --host ${MARIADB_HOST:-mariadb} --user allthethings --password password --database allthethings --compress --verbose 3 --long-query-guard 999999 --no-locks --compress-protocol --order-by-primary --outputdir /exports/mariadb
+mydumper --threads 32 --omit-from-file /app/data-imports/scripts/dump_mariadb_omit_tables.txt --exit-if-broken-table-found --tz-utc --host ${MARIADB_HOST:-mariadb} --user allthethings --password password --database allthethings --compress --verbose 3 --long-query-guard 999999 --no-locks --compress-protocol --order-by-primary --format CSV --include-header --build-empty-files --outputdir /exports/mariadb
 
 # Not as acutely necessary to verify gzip integrity here (compared to elasticdump scripts), but might as well.
 time ls *.gz | parallel 'echo {}: $(zcat {} | wc -l)'
