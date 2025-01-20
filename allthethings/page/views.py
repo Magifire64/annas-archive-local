ecdc8117ad926e13c6a311ef8fb4a41776861de8
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2025-01-20 00:00:00 +0000

    zzz

--- allthethings/page/views.py
+++ allthethings/page/views.py
@@ -973,7 +973,7 @@ def contact_page():
     account_id = allthethings.utils.get_account_id(request.cookies)
     if account_id is None:
         return render_template("page/login_to_view.html", header_active="")
-    return render_template("page/contact.html", header_active="", AA_EMAIL=AA_EMAIL)
+    return render_template("page/contact.html", header_active="", AA_EMAIL=AA_EMAIL.replace('@', f"+{account_id}@"))
 
 @page.get("/fast_download_no_more")
 @allthethings.utils.public_cache(minutes=5, cloudflare_minutes=60*3)
