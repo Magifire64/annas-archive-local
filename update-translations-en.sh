a1937d7a4fe5182c848c498ad899d8a689fd85c9
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2025-07-23 00:00:00 +0000

    zzz

--- update-translations-en.sh
+++ update-translations-en.sh
@@ -3,7 +3,7 @@
 set -Eeuxo pipefail
 
 # Convert the source HTML files into the translatable versions
-./bin/translate-html --in-place ./allthethings/**/templates/**/*.html.j2
+./run cmd bin/translate-html --in-place ./allthethings/**/templates/**/*.html.j2
 
 # Some of these change their output when run multiple times
 # TODO: --sort-output, to sort by msgid instead of file
